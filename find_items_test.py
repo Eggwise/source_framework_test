1475a5f52a17bd5af00bd170bb3b85020032f083
eggwise <dev@eggwise.com>
2016-10-21 10:57:00 +0200

    more example code

--- find_items_test.py
+++ find_items_test.py
@@ -1,16 +1,18 @@
 import source_framework
 import logging
 
-logging.basicConfig(level=1)
-
-
-
+# logging.basicConfig(level=1)
 
 
 source_framework.init()
 
 #scope to the useless items
 useless_items = source_framework.find().useless.items
+
+cool_useless_item = useless_items.cool.one
+# cool_useless_item =  source_framework.find().cool.useless.items.one
+# cool_useless_item =  source_framework.find().useless.cool.one
+
 
 #list them
 useless_items.list()
